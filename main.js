35648062518acca3ba911aebc230bb3f8a20eb86
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-04-25 21:39:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload() {
-
+clown_nose=loadImage("https://i.postimg.cc/9FFNg3c5/png-transparent-moustache-mustache-hair-beard-mustache-png.png");
 }
 
 function setup() {
@@ -14,6 +16,7 @@ function setup() {
 
 function draw() {
 image(video, 0, 0, 300, 300);
+image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot() {
@@ -29,7 +32,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y+5;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" +  noseY);
+        
     }
 }
